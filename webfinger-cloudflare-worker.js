d34e11abd271bfda90d0412cd1be5478e988da5f
Kurt Seifried <kurt@seifried.org>
2022-11-09 11:38:26 -0800

    Added todo and line wraps

--- webfinger-cloudflare-worker.js
+++ webfinger-cloudflare-worker.js
@@ -1,8 +1,11 @@
-// So you want to use webfinger to map every user at your site so they can add @ to their email (e.g. @kurt@seifried.org) and use that as a Mastodon ID to redirect to whatever Mastodon ID they're actually using?
+// So you want to use webfinger to map every user at your site so they can add @ to their email (e.g. @kurt@seifried.org) and use 
+// that as a Mastodon ID to redirect to whatever Mastodon ID they're actually using?
 
-// Simply deploy webfinger-cloudflare-worker.js as a Cloudflare Worker using a route such as https://seifried.org/.well-known/webfinger* and populate the redirectMap data
+// Simply deploy webfinger-cloudflare-worker.js as a Cloudflare Worker using a route such as https://seifried.org/.well-known/webfinger* 
+// and populate the redirectMap data
 
-// This has obvious scale issues (a cloudflare worker can only be 1MB in size after compression), but it should work up to a few hundred (thousands?) users.
+// This has obvious scale issues (a cloudflare worker can only be 1MB in size after compression), but it should work up to a few hundred 
+// (thousands?) users.
 
 // The obvious solution if you need to scale past this is to use Cloudflare KV to simply map a key of account name to the JSON data. PR's welcome.
                                                                                                                                        
@@ -15,6 +18,8 @@
 // Test cases:
 // https://seifried.org/.well-known/webfinger?resource=acct:kurt@seifried.org
 // https://seifried.org/.well-known/webfinger?resource=acct:kurt@seifried.org2
+
+// TODO: deployment via wrangler bits
 
 const redirectMap = new Map([
   ['kurt@seifried.org', '@kurtseifried@mastodon.social'],
