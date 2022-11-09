b44feb88f6a810cf5ed9e160f0e53a2d5991e9dd
Kurt Seifried <kurt@seifried.org>
2022-11-09 11:26:40 -0800

    Better string handling

--- webfinger-cloudflare-worker.js
+++ webfinger-cloudflare-worker.js
@@ -8,14 +8,15 @@
 // https://seifried.org/.well-known/webfinger?resource=acct:kurt@seifried.org2
 
 const redirectMap = new Map([
-  ['acct:kurt@seifried.org', '{"subject":"acct:kurt@seifried.org","aliases":["https://mastodon.social/@kurtseifried","https://mastodon.social/users/kurtseifried"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://mastodon.social/@kurtseifried"},{"rel":"self","type":"application/activity+json","href":"https://mastodon.social/users/kurtseifried"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://mastodon.social/authorize_interaction?uri={uri}"}]}'],
-  ['acct:test@seifried.org', '{"moredata": "here"}'],
+  ['kurt@seifried.org', '{"subject":"acct:kurt@seifried.org","aliases":["https://mastodon.social/@kurtseifried","https://mastodon.social/users/kurtseifried"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://mastodon.social/@kurtseifried"},{"rel":"self","type":"application/activity+json","href":"https://mastodon.social/users/kurtseifried"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://mastodon.social/authorize_interaction?uri={uri}"}]}'],
+  ['test@seifried.org', '{"moredata": "here"}'],
 ]);
 
 async function handleRequest(request) {
   const requestURL = new URL(request.url);
   const resourceKey = requestURL.searchParams.get('resource');
-  const resourceData = redirectMap.get(resourceKey);
+  const email = resourceKey.replace("acct:", "");
+  const resourceData = redirectMap.get(email);
   if (resourceData) {
 const jsonData = resourceData
     return new Response(jsonData, {
