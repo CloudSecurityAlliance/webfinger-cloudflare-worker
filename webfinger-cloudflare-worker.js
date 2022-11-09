73ac24244fa5b6675c08b5f164b878e38fa61496
Kurt Seifried <kurt@seifried.org>
2022-11-09 11:36:43 -0800

    Final cleanup of json creation

--- webfinger-cloudflare-worker.js
+++ webfinger-cloudflare-worker.js
@@ -2,14 +2,15 @@
 // Based off of https://developers.cloudflare.com/workers/examples/return-json/
 // and whatever the third(?) google result for "cloudflare worker query string" was
 // Also thanks to https://jsoneditoronline.org/, cut and paste your webfinger json in and hit the minify button to get a string
+// And https://docs.joinmastodon.org/spec/webfinger/
 
 // Test cases:
 // https://seifried.org/.well-known/webfinger?resource=acct:kurt@seifried.org
 // https://seifried.org/.well-known/webfinger?resource=acct:kurt@seifried.org2
 
 const redirectMap = new Map([
-  ['kurt@seifried.org', '{"subject":"acct:kurt@seifried.org","aliases":["https://mastodon.social/@kurtseifried","https://mastodon.social/users/kurtseifried"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://mastodon.social/@kurtseifried"},{"rel":"self","type":"application/activity+json","href":"https://mastodon.social/users/kurtseifried"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://mastodon.social/authorize_interaction?uri={uri}"}]}'],
-  ['test@seifried.org', '{"moredata": "here"}'],
+  ['kurt@seifried.org', '@kurtseifried@mastodon.social'],
+  ['test@seifried.org', '@test@tld'],
 ]);
 
 async function handleRequest(request) {
@@ -18,17 +19,20 @@ async function handleRequest(request) {
   const email = resourceKey.replace("acct:", "");
   const resourceData = redirectMap.get(email);
   if (resourceData) {
-const jsonData = resourceData
+    const resourceArray = resourceData.split("@");
+    const username = resourceArray[1];
+    const hostname = resourceArray[2];
+    const jsonData = '{"subject":"acct:' + email + '","aliases":["https://' + hostname + '/@' + username + '","https://' + hostname + '/users/' + username + '"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://' + hostname + '/@' + username + '"},{"rel":"self","type":"application/activity+json","href":"https://' + hostname + '/users/' + username + '"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://' + hostname + '/authorize_interaction?uri={uri}"}]}';
     return new Response(jsonData, {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
-        },
-        });
-      }
+      },
+    });
+  }
   // If request not in map, return empty string and 404
   return new Response('', {
-        status: 404
-      });
+    status: 404
+  });
 }
 
 addEventListener('fetch', async event => {
