034b97204fe363378b1669fbfd4642fc60c13d1c
Kurt Seifried <kurt@seifried.org>
2022-11-12 20:46:01 -0800

    Updated description

--- webfinger-cloudflare-worker.js
+++ webfinger-cloudflare-worker.js
@@ -1,5 +1,5 @@
-// So you want to use webfinger to map every user at your site so they can add @ to their email (e.g. @kurt@seifried.org) and use 
-// that as a Mastodon ID to redirect to whatever Mastodon ID they're actually using?
+// So you want to use webfinger to map every user at your site so they use their email 
+// as a Mastodon search term to redirect to whatever Mastodon ID they're actually using?
 
 // Simply deploy webfinger-cloudflare-worker.js as a Cloudflare Worker using a route such as https://seifried.org/.well-known/webfinger* 
 // and populate the redirectMap data
